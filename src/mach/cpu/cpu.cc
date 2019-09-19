27f4648387d58eb7c822a089a4666944547f9da0
Anton Ihonen <anton@ihonen.net>
2019-09-19 22:15:16 +0300

    CPU: Add new initial values to CPU_init()

--- src/mach/cpu/cpu.cc
+++ src/mach/cpu/cpu.cc
@@ -14,11 +14,6 @@ void CPU::CPU_init()
 {
     mem = {0};
     reg = {0};
-    IME_flag = 0x00;
-    mem[HWREG_IF_ADDR] = 0x00;
-    mem[HWREG_IF_ADDR] = 0x00;
-    PC = 0x0100;
-    SP = 0xFFFE;
     curr_instr = nullptr;
     curr_interrupt = nullptr;
     branch_taken = false;
@@ -28,6 +23,48 @@ void CPU::CPU_init()
     is_stopped = false;
     clock_cycles = 0;
     is_interrupted = false;
+
+    IME_flag = 0x00;
+    mem[HWREG_IF_ADDR] = 0x00;
+    mem[HWREG_IF_ADDR] = 0x00;
+    AF = 0x01B0;
+    BC = 0x0013;
+    DE = 0x00D8;
+    HL = 0x014D;
+    SP = 0xFFFE;
+    mem[0xFF05] = 0x00; // TIMA
+    mem[0xFF06] = 0x00; // TMA
+    mem[0xFF07] = 0x00; // TAC
+    mem[0xFF10] = 0x80; // NR10
+    mem[0xFF11] = 0xBF; // NR11
+    mem[0xFF12] = 0xF3; // NR12
+    mem[0xFF14] = 0xBF; // NR14
+    mem[0xFF16] = 0x3F; // NR21
+    mem[0xFF17] = 0x00; // NR22
+    mem[0xFF19] = 0xBF; // NR24
+    mem[0xFF1A] = 0x7F; // NR30
+    mem[0xFF1B] = 0xFF; // NR31
+    mem[0xFF1C] = 0x9F; // NR32
+    mem[0xFF1E] = 0xBF; // NR33
+    mem[0xFF20] = 0xFF; // NR41
+    mem[0xFF21] = 0x00; // NR42
+    mem[0xFF22] = 0x00; // NR43
+    mem[0xFF23] = 0xBF; // NR44
+    mem[0xFF24] = 0x77; // NR50
+    mem[0xFF25] = 0xF3; // NR51
+    mem[0xFF26] = 0xF1; // NR52
+    mem[0xFF40] = 0x91; // LCDC
+    mem[0xFF42] = 0x00; // SCY
+    mem[0xFF43] = 0x00; // SCX
+    mem[0xFF45] = 0x00; // LYC
+    mem[0xFF47] = 0xFC; // BGP
+    mem[0xFF48] = 0xFF; // OBP0
+    mem[0xFF49] = 0xFF; // OBP1
+    mem[0xFF4A] = 0x00; // WY
+    mem[0xFF4B] = 0x00; // WX
+    mem[0xFFFF] = 0x00; // IE
+
+    PC = 0x0100;
 }
 
 void CPU::execute(const uint8_t* instruction)
