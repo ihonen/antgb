ba602667652517eb77a5768a1aff943dca2b9836
Anton Ihonen <anton.ihonen@gmail.com>
2020-01-20 15:34:27 +0200

    PPU: Add GraphicsFetcher class

--- src/mach/graphicsfetcher.hh
+++ src/mach/graphicsfetcher.hh
@@ -1,5 +1,28 @@
 #ifndef GRAPHICSFETCHER_HH
 #define GRAPHICSFETCHER_HH
 
+#include "background.hh"
+#include "mmu.hh"
+#include "ppuregisters.hh"
+#include "tile.hh"
+#include "sprite.hh"
+
+class GraphicsFetcher
+{
+public:
+    GraphicsFetcher(MMU& mmu, PPUReg& reg);
+
+    void fetch_all();
+
+    vector<Sprite> sprite_buffer;
+    Background background_buffer;
+    Background window_buffer;
+
+    MMU& mmu;
+    PPUReg& reg;
+
+    Background::TileMap bg_tilemap;
+    Background::TileMap window_tilemap;
+};
 
 #endif // GRAPHICSFETCHER_HH
