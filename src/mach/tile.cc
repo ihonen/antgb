dd3115572055244b59bffec06bb34094319f2b86
Anton Ihonen <anton@ihonen.net>
2020-01-21 22:57:53 +0200

    PPU: Don't read tile row data backwards

--- src/mach/tile.cc
+++ src/mach/tile.cc
@@ -9,7 +9,7 @@ uint8_t Tile::get_pixel(uint8_t tile_x, uint8_t tile_y)
 
     uint8_t* row_low = &data[tile_y * 2];
     uint8_t* row_high = row_low + 1;
-    uint8_t low_bit = (*row_low & (0x01 << tile_x)) >> tile_x;
-    uint8_t high_bit = (*row_high & (0x01 << tile_x)) >> (tile_x);
+    uint8_t low_bit = (*row_low & (0x01 << (7 - tile_x))) >> ((7 - tile_x));
+    uint8_t high_bit = (*row_high & (0x01 << (7 - tile_x))) >> ((7 - tile_x));
     return (high_bit << 1) | low_bit;
 }
