6af1935ad0ce409c871e83e0549545711246ee78
Anton Ihonen <anton@ihonen.net>
2020-05-06 19:20:56 +0300

    Timer: Fix bugs

--- src/mach/timer.cc
+++ src/mach/timer.cc
@@ -31,27 +31,27 @@ void Timer::emulate_timer(uint64_t cpu_cycles)
 {
     timer.unemulated_cpu_cycles += cpu_cycles;
 
-    if (!(mem->hff07_tac & TimerEnable))
+    if ((mem->hff07_tac & (0x01 << TimerEnable)) == 0x00)
     {
         timer.unemulated_cpu_cycles = 0;
         return;
     }
 
     uint64_t clk_select = mem->hff07_tac & (timer.CLK_SELECT_MASK);
-    uint64_t cpu_cycles_per_tick = Cpu::CLK_FREQ_Hz / timer.FREQ_DIVIDER[clk_select];
+    uint64_t cpu_cycles_per_tick = timer.FREQ_DIVIDER[clk_select];
 
     while (timer.unemulated_cpu_cycles >= cpu_cycles_per_tick)
     {
-        cout << mem->hff05_tima << endl;
+        timer.unemulated_cpu_cycles -= cpu_cycles_per_tick;
         if (mem->hff05_tima == 0xFF)
         {
             mem->hff05_tima = mem->hff06_tma;
             irc->request_interrupt(InterruptController::TimerInterrupt);
+            return;
         }
         else
         {
             ++(mem->hff05_tima);
         }
-        timer.unemulated_cpu_cycles -= cpu_cycles_per_tick;
     }
 }
