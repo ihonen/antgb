248cdd6af6b91bb8c18d4d2f9784433dc57458e8
Anton Ihonen <anton@ihonen.net>
2020-05-06 21:09:00 +0300

    Serial: Fix bugs

--- src/mach/serial.cc
+++ src/mach/serial.cc
@@ -11,12 +11,13 @@ void Serial::emulate(uint64_t cpu_cycles)
 {
     if (mem->hff01_sb != 0x00 && mem->hff02_sc == 0x81)
     {
-        cout << (char)mem->hff01_sb << endl;
+        cout << (char)mem->hff01_sb;
+        std::cout << std::flush;
         mem->hff01_sb = 0x00;
         cpu_cycles_left_in_transfer = CPU_CYCLES_PER_BYTE;
     }
 
-    if (cpu_cycles_left_in_transfer <= cpu_cycles)
+    if (cpu_cycles_left_in_transfer <= cpu_cycles && cpu_cycles_left_in_transfer != 0)
     {
         irc->request_interrupt(InterruptController::SerialInterrupt);
         cpu_cycles_left_in_transfer = 0;
