e63c302e691b5f01a8003795e4ce05a7c1bd2ace
Anton Ihonen <anton@ihonen.net>
2021-06-10 18:34:58 +0300

    I don't know how to describe this one

--- src/gui/widgets/mainwindow.cc
+++ src/gui/widgets/mainwindow.cc
@@ -225,37 +225,37 @@ void MainWindow::load_rom_act()
                                                  ".",
                                                  "Game Boy ROMs (*.gb)");
     */
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\cpu_instrs.gb");
+    // QString filepath("cpu_instrs.gb");
     QString filepath("tetris_jue_v1_1.gb");
 
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\01-special.gb");
+    // QString filepath("01-special.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\02-interrupts.gb");
+    // QString filepath("02-interrupts.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\03-op sp,hl.gb");
+    // QString filepath("03-op sp,hl.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\04-op r,imm.gb");
+    // QString filepath("04-op r,imm.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\05-op rp.gb");
+    // QString filepath("05-op rp.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\06-ld r,r.gb");
+    // QString filepath("06-ld r,r.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\07-jr,jp,call,ret,rst.gb");
+    // QString filepath("07-jr,jp,call,ret,rst.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\08-misc instrs.gb");
+    // QString filepath("08-misc instrs.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\09-op r,r.gb");
+    // QString filepath("09-op r,r.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\10-bit ops.gb");
+    // QString filepath("10-bit ops.gb");
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\cpu_instrs\\individual\\11-op a,(hl).gb");
+    // QString filepath("11-op a,(hl).gb");
 
     // PASSED:
-    // QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\instr_timing\\instr_timing.gb");
+    // QString filepath("instr_timing.gb");
 
     // NOT PASSED:
-    //QString filepath("C:\\Users\\anton\\Desktop\\antgb\\testbin\\blargg\\interrupt_time\\interrupt_time.gb");
+    //QString filepath("interrupt_time.gb");
 
     emuwidget->load_rom(filepath);
 }
