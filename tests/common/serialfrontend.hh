97f6d30bd5bcbeba79ca3c0f1a8d3ae2ff45bba6
Anton Ihonen <anton@ihonen.net>
2021-06-12 12:13:59 +0300

    Tests: Fix compiler warning from missing parameter in SerialFrontend::render_callback

--- tests/common/serialfrontend.hh
+++ tests/common/serialfrontend.hh
@@ -3,8 +3,14 @@
 class SerialFrontend : public iFrontend
 {
 public:
-    virtual void render_callback(const iFrontend::Pixels& pixels) {}
-    virtual void serial_callback(uint8_t byte) { received_ += static_cast<char>(byte); }
+    virtual void render_callback(const iFrontend::Pixels&)
+	{	
+	}
+	
+    virtual void serial_callback(uint8_t byte)
+	{
+		received_ += static_cast<char>(byte);
+	}
 
     const std::string& get_received() { return received_; }
 protected:
